52f9d67a0fce76dda513d730f2870265107d2d83
xSmair <simonmair01@gmx.at>
2020-09-28 13:06:34 +0200

    init Uebung3

---
+++ src/Uebung3/NumberTest.java
@@ -0,0 +1,5 @@
+package Uebung3;
+
+public interface NumberTest {
+    boolean testNumber(int number);
+}
---
+++ src/Uebung3/NumberTester.java
@@ -0,0 +1,24 @@
+package Uebung3;
+
+public class NumberTester {
+    public NumberTester(String fileName){
+        //TODO
+    }
+
+    public void setOddEvenTester(NumberTester oddTester){
+
+    }
+
+    public void setPrimeTester(NumberTester primeTester){
+
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester){
+
+    }
+
+    public void testFile(){
+
+    }
+}
+
