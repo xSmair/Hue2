e10c5cea62f9ca38bd96623395b3f228748b0605
xSmair <simonmair01@gmx.at>
2020-09-28 13:02:32 +0200

    init

---
+++ src/Uebung2/HalloJavaMitForEach.java
@@ -0,0 +1,19 @@
+package Uebung2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HalloJavaMitForEach {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+
+        for (int i = 0; i < stringList.size(); i++) {
+            System.out.println(stringList.get(i));
+        }
+
+        stringList.forEach((String s) -> System.out.println(s));
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+
+    }
+}
