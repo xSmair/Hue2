a9dbe07e9bbdcf33cc99c070e36530b265dc1457
xSmair <simonmair01@gmx.at>
2020-09-28 13:26:02 +0200

    Uebung3 basic algorithm

--- src/Uebung3/NumberTester.java
+++ src/Uebung3/NumberTester.java
@@ -1,8 +1,24 @@
 package Uebung3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester {
     public NumberTester(String fileName){
-        //TODO
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+            int length = Integer.parseInt(line);
+            for (int i = 0; i < length; i++) {
+                String[] temp = br.readLine().split(" ");
+                int type = Integer.parseInt(temp[0]);
+                int number = Integer.parseInt(temp[1]);
+            }
+        }catch (IOException IOException){
+
+        }
     }
 
     public void setOddEvenTester(NumberTester oddTester){
